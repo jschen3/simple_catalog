9b41420bbb33c183e27df369c3a6dbbf73ea2c38
Jimmy Chen <chen.s.jimmy@gmail.com>
2016-03-09 22:34:20 -0700

    commiting progress in challenge

--- js/invoice-app.js
+++ js/invoice-app.js
@@ -1,9 +1,12 @@
 var invoiceApp = angular.module("invoiceApp" ,[]);
 invoiceApp.controller("InvoiceCtrl", function($scope){
+    $scope.total=(13.00).toFixed(2);
+    //$scope.total=$scope.calculateTotal();
+    $scope.info={'name':'John Doe','address':'The LandMark at One Market','city':'San Francisco, CA','date':''};
     $scope.invoice=[
-        {'id':'0', 'image':'images/banana.jpeg', 'name':'bananas','price':'6.00','quantity':'1', 'total':'6'},
-        {'id':'1', 'image':'images/apple.jpeg', 'name':'apples','price':'3.00', 'quantity':'1', 'total':'3'},
-        {'id':'2', 'image':'images/pear.jpeg', 'name':'pears', 'price':'4.00', 'quantity':'1', 'total':'4'}
+        {'id':'0', 'image':'images/banana.jpeg', 'name':'bananas','price':'6.00','quantity':'1', 'total':'6.00'},
+        {'id':'1', 'image':'images/apple.jpeg', 'name':'apples','price':'3.00', 'quantity':'1', 'total':'3.00'},
+        {'id':'2', 'image':'images/pear.jpeg', 'name':'pears', 'price':'4.00', 'quantity':'1', 'total':'4.00'}
     ];
     $scope.catalog=[
         {'id':'0', 'image':'images/banana.jpeg', 'name':'bananas','price':'6.00'},
@@ -26,7 +29,8 @@ invoiceApp.controller("InvoiceCtrl", function($scope){
                 break;
             }
         }
-        $scope.invoice[index].total=$scope.invoice[index].quantity * $scope.invoice[index].price;
+        $scope.invoice[index].total=($scope.invoice[index].quantity * $scope.invoice[index].price).toFixed(2);
+        calculateTotal();
     };
     $scope.removeRow = function(id){
         var index=-1;
@@ -38,7 +42,42 @@ invoiceApp.controller("InvoiceCtrl", function($scope){
             }
         }
         $scope.invoice.splice(index, 1);
+        calculateTotal();
     };
-
-
+    $scope.addItem = function(id){
+        var index=-1;
+        var catalogItems = eval($scope.catalog);
+        var invoiceItems = eval($scope.invoice);
+        for (var i=0; i<catalogItems.length; i++){
+            if (catalogItems[i].id===id){
+                index=i;
+                break;
+            }
+        }
+        var invoiceId=-1;
+        for (var z=0; z<invoiceItems.length; z++){
+            if (invoiceItems[z].id===id){
+                invoiceId=z;
+                break;
+            }
+        }
+        if (invoiceId===-1){
+            $scope.invoice
+                .push({'id':catalogItems[index].id,'image':catalogItems[index].image, 'name':catalogItems[index].name,
+                'price':catalogItems[index].price, 'quantity':'1', 'total':catalogItems[index].price});
+        }
+        else{
+            $scope.invoice[invoiceId].quantity=parseInt($scope.invoice[invoiceId].quantity)+parseInt(parseInt(1));
+            $scope.invoice[invoiceId].total=(parseInt($scope.invoice[invoiceId].quantity)*parseInt($scope.invoice[invoiceId].price)).toFixed(2);
+        }
+        calculateTotal();
+    };
+    calculateTotal = function(){
+        var sum=0;
+        var invoiceItems = eval($scope.invoice);
+        for (var i=0; i<invoiceItems.length; i++){
+            sum+=parseInt(invoiceItems[i].total);
+        }
+        $scope.total=sum.toFixed(2);
+    };
 });
