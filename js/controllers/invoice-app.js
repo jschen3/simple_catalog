ab41613bf2388b0bb202ebdc9c04cfa4c1fea4b6
Jimmy Chen <chen.s.jimmy@gmail.com>
2016-03-14 20:57:33 -0600

    added comments to code

--- js/controllers/invoice-app.js
+++ js/controllers/invoice-app.js
@@ -1,3 +1,6 @@
+/*
+ * Controller class for the invoiceApp. Calls utility functions to manage the app.
+ */
 var app=angular.module('invoiceApp',[]);
 angular.module('invoiceApp').controller('InvoiceCtrl', ['$scope', 'invoiceFactory', 'catalogFactory', function($scope, invoiceFactory, catalogFactory){
     var d=new Date();
@@ -7,20 +10,38 @@ angular.module('invoiceApp').controller('InvoiceCtrl', ['$scope', 'invoiceFactor
     $scope.info.id=Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
     $scope.invoice=invoiceFactory.getInvoice();
     $scope.catalog=catalogFactory.getCatalog();
+    /*
+     * removes an item from the invoice.
+     * @param id of the item to remove.
+     */
     $scope.removeRow = function(id){
         invoiceFactory.removeItem(id);
         $scope.total=invoiceFactory.getTotal();
     };
+    /*
+     * adds an item to the invoice from the catalog.
+     * @param id from the catalog of the item added to the invoice.
+     */
     $scope.addItem = function(id){
         var index=-1;
         var item=catalogFactory.getItem(id);
         invoiceFactory.addItem(item);
         $scope.total=invoiceFactory.getTotal();
     };
+    /*
+     * updates the values of an item in the invoice.
+     * @param id id of the item from the invoice changed
+     * @param price price of the item in the invoice changed
+     * @param quantity of the item in the invoice changed
+     */
     $scope.updateRow = function(id, price, quantity){
         invoiceFactory.updateRow(id, price, quantity);
         $scope.total=invoiceFactory.getTotal();
     }
+    /*
+     * utility function to prepare invoice for printing.
+     * TODO: Fix bug where printing freezes javascript.
+     */
     $scope.print = function(){
         var restorepage = document.body.innerHTML;
 	    var printcontent = document.getElementById("invoice-main").innerHTML;
